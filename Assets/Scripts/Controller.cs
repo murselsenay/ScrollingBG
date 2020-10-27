f54e47ed62ee14db1d70b6cf36bbbcf27258a388
murselsenay <murselsenay@gmail.com>
2020-10-27 22:14:03 +0300

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Controller : MonoBehaviour
+{
+    public static Controller instance;
+    [Header("Background Objects")]
+    public List<GameObject> bg = new List<GameObject>();
+    public GameObject bgContainer;
+    public GameObject pieceOfBg;
+    private int bgCount = 2;
+    private float yOffset = 0;
+
+    [Header("Background Sprites")]
+    public Sprite blue;
+    public Sprite green;
+    internal bool canSwitchColor = false;
+    internal int bgOrder = 0;
+
+    [Header("Timer")]
+    public Text timerText;
+    public int countDown = 10;
+    public Button resetBtn;
+    internal int resetBtnStatus;
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBg();
+        ResetBtnDisabled();
+        timerText.text = countDown.ToString();
+        StartCoroutine(Timer(countDown));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetTimer()
+    {
+
+        timerText.text = countDown.ToString();
+        ResetBtnDisabled();
+        bgOrder = 0;
+        StartCoroutine(Timer(countDown));
+    }
+
+    public void CreateBg()
+    {
+        for (int i = 0; i < bgCount; i++)
+        {
+            bg.Add(Instantiate(pieceOfBg, new Vector2(0f, 0f + yOffset), Quaternion.identity));
+            float height = bg[i].GetComponent<BoxCollider2D>().bounds.size.y;
+            yOffset += height;
+        }
+
+    }
+
+    public IEnumerator Timer(int _countDown)
+    {
+        _countDown = countDown;
+        while (true)
+        {
+            if (_countDown >= 0)
+            {
+                yield return new WaitForSeconds(0.5f);
+                timerText.gameObject.SetActive(true);
+                yield return new WaitForSeconds(0.5f);
+                timerText.gameObject.SetActive(false);
+                _countDown -= 1;
+                timerText.text = _countDown.ToString();
+                canSwitchColor = false;
+            }
+            else
+            {
+                
+                countDown = _countDown;
+                break;
+            }
+        }
+
+
+    }
+    public void ResetBtnEnabled()
+    {
+        resetBtn.interactable = true;
+        
+    }
+    public void ResetBtnDisabled()
+    {
+        resetBtn.interactable = false;
+
+    }
+    public void SwitchColor(Sprite _sprite, int _order)
+    {
+        bg[_order].GetComponent<SpriteRenderer>().sprite = _sprite;
+    }
+}
---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float speed = -1.5f;
+
+    [SerializeField]
+    private bool cancelScrolling;
+
+
+
+
+    private BoxCollider2D pieceOfBgCollider;
+    private float pieceOfBgSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0f, speed);
+
+        pieceOfBgCollider = GetComponent<BoxCollider2D>();
+        pieceOfBgSize = pieceOfBgCollider.bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cancelScrolling)
+            rb.velocity = Vector2.zero;
+        else
+            rb.velocity = new Vector2(0f, speed);
+
+
+        if (transform.position.y < -pieceOfBgSize)
+        {
+            if (Controller.instance.countDown <= 0)
+            {
+                if (Controller.instance.bgOrder == 0)
+                {
+                    Controller.instance.SwitchColor(Controller.instance.green, 0);
+                    Controller.instance.bgOrder += 1;
+                    Debug.Log("Color Switched");
+                }
+                else if (Controller.instance.bgOrder == 1)
+                {
+                    Controller.instance.SwitchColor(Controller.instance.green, 1);
+                    Controller.instance.bgOrder = 0;
+                    Controller.instance.countDown = 10;
+                    Controller.instance.ResetBtnEnabled();
+                    Debug.Log("Color Switched");
+                }
+               
+            }
+           
+            SwitchBGPosition();
+        }
+    }
+
+
+    void SwitchBGPosition()
+    {
+        Vector2 pieceOfBgOffset = new Vector2(0f, pieceOfBgSize * 2f);
+        transform.position = (Vector2)transform.position + pieceOfBgOffset;
+    }
+
+}
